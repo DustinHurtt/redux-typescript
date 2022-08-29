75df682eac55ae956dbf686f60195941c652c1be
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-08-29 17:05:31 -0400

    project complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,21 @@
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+// 1) Import ReactDOM library
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+ 
+// 2) Get a reference to the div with ID root
+const el = document.getElementById("root");
+ 
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el!);
+ 
+// 4) Create a component
+// const App = () => {
+//   return (
+//     <div>
+//       <UserSearch/>
+//     </div>
+//   );
+// };
+ 
+// 5) Show the component on the screen
+root.render(<App />);
